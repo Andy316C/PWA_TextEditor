dc9f6579193e586502817f2567b37380203e97cb
andrew clark <andy-clark316@hotmail.com>
2023-06-25 17:37:02 +1000

    updated webpackages

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,5 +1,5 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
+import { getDb, postDb, putDb} from './database';
 import { header } from './header';
 
 export default class {
@@ -10,7 +10,7 @@ export default class {
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
-
+    
     this.editor = CodeMirror(document.querySelector('#main'), {
       value: '',
       mode: 'javascript',
@@ -21,12 +21,16 @@ export default class {
       indentUnit: 2,
       tabSize: 2,
     });
-
+   
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      console.log(data);
+      
+        this.editor.setValue(data.todo || localData || header);
+      
+      // this.editor.setValue(data || localData || header);
     });
 
     this.editor.on('change', () => {
