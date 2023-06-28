0bf19fb2195fe611b243764fe8afba1c4196be26
andrew clark <andy-clark316@hotmail.com>
2023-06-29 09:30:39 +1000

    removed console.logs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -21,13 +21,13 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({  todo: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('Data saved to the database', result);
 };
 
 // // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   
-  console.log('GET from the database');
+  // console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
